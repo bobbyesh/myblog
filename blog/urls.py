76b1ef41a1029fc0e6668a60404ff6a4093f55a8
Bobby Eshleman <eshleman@pdx.edu>
2017-02-17 19:30:53 -0800

    Added HomePageView

--- blog/urls.py
+++ blog/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from blogapp.views import HomePageView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', HomePageView.as_view()),
 ]
